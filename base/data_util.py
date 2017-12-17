f28940bd127e1ecfa4b3b8f4b086ee9a58e01b48
rtygbwwwerr <rtygbwwwerr@163.com>
2017-12-18 00:23:11 +0800

    Update data_util.py

--- base/data_util.py
+++ base/data_util.py
@@ -11,22 +11,29 @@ from datetime import datetime
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
+def isDigitType(value):
+	is_flg = isinstance(value, int) or isinstance(value, float) or \
+			isinstance(value, np.float) or isinstance(value, np.int) or \
+			isinstance(value, np.float32) or isinstance(value, np.int32) or \
+			isinstance(value, np.float64) or isinstance(value, np.int64) or \
+			isinstance(value, np.float128) or isinstance(value, np.int16) or \
+			isinstance(value, np.float16) or isinstance(value, np.int8)
+	return is_flg
 
 def floatrange(start,stop,steps):
 	''' Computes a range of floating value.
 	   
-	    Input:
-	        start (float)  : Start value.
-	        end   (float)  : End value
-	        steps (integer): Number of values
+		Input:
+			start (float)  : Start value.
+			end   (float)  : End value
+			steps (integer): Number of values
 	   
-	    Output:
-	        A list of floats
+		Output:
+			A list of floats
 	   
-	    Example:
-	        >>> print floatrange(0.25, 1.3, 5)
-	        [0.25, 0.51249999999999996, 0.77500000000000002, 1.0375000000000001, 1.3]
+		Example:
+			>>> print floatrange(0.25, 1.3, 5)
+			[0.25, 0.51249999999999996, 0.77500000000000002, 1.0375000000000001, 1.3]
 	'''
 	return [start+float(i)*(stop-start)/(float(steps)-1) for i in range(steps)]
 
@@ -122,8 +129,99 @@ def detect_intersection_ids(df_train, df_test):
 	print "train id num:%d, test id num:%d, intersection id num:%d."%(len(train_ids), len(test_ids), intersection_count)
 	
 	return intersection_count == len(test_ids)
+
+def get_arrays_lengths(a, EOS_ID):
+	lens = np.argwhere(a==EOS_ID)[:,1] + 1
+	if EOS_ID is None:
+		lens = get_sequence_lengths(a)
+	return lens
 	
+
+def get_sequence_lengths(sequences):
+# 	for s in sequences:
+# 		s.index(end_id)
+	lengths = np.asarray([len(s) for s in sequences], dtype=np.int32)
+	return lengths
+
+def pad_sequences(sequences, maxlen=None, dtype=np.float32,
+				  padding='post', truncating='post', value=0.):
+	'''Pads each sequence to the same length: the length of the longest
+	sequence.
+		If maxlen is provided, any sequence longer than maxlen is truncated to
+		maxlen. Truncation happens off either the beginning or the end
+		(default) of the sequence. Supports post-padding (default) and
+		pre-padding.
+
+		Args:
+			sequences: list of lists where each element is a sequence
+			maxlen: int, maximum length
+			dtype: type to cast the resulting sequence.
+			padding: 'pre' or 'post', pad either before or after each sequence.
+			truncating: 'pre' or 'post', remove values from sequences larger
+			than maxlen either in the beginning or in the end of the sequence
+			value: float, value to pad the sequences to the desired value.
+		Returns
+			x: numpy array with dimensions (number_of_sequences, maxlen)
+			lengths: numpy array with the original sequence lengths
+	'''
+	lengths = np.asarray([len(s) for s in sequences], dtype=np.int64)
+
+	nb_samples = len(sequences)
+	if maxlen is None:
+		maxlen = np.max(lengths)
+
+	# take the sample shape from the first non empty sequence
+	# checking for consistency in the main loop below.
+	sample_shape = tuple()
+	for s in sequences:
+		if len(s) > 0:
+			sample_shape = np.asarray(s).shape[1:]
+			break
+
+	x = (np.ones((nb_samples, maxlen) + sample_shape) * value).astype(dtype)
+	for idx, s in enumerate(sequences):
+		if len(s) == 0:
+			continue  # empty list was found
+		if truncating == 'pre':
+			trunc = s[-maxlen:]
+		elif truncating == 'post':
+			trunc = s[:maxlen]
+		else:
+			raise ValueError('Truncating type "%s" not understood' % truncating)
+
+		# check `trunc` has expected shape
+		trunc = np.asarray(trunc, dtype=dtype)
+		if trunc.shape[1:] != sample_shape:
+			raise ValueError('Shape of sample %s of sequence at position %s is different from expected shape %s' %
+							 (trunc.shape[1:], idx, sample_shape))
+
+		if padding == 'post':
+			x[idx, :len(trunc)] = trunc
+		elif padding == 'pre':
+			x[idx, -len(trunc):] = trunc
+		else:
+			raise ValueError('Padding type "%s" not understood' % padding)
+	return x, lengths
+   
+def sparse_tuple_from(sequences, dtype=np.int32):
+	"""Create a sparse representention of x.
+	Args:
+		sequences: a list of lists of type dtype where each element is a sequence
+	Returns:
+		A tuple with (indices, values, shape)
+	"""
+	indices = []
+	values = []
 	
+	for n, seq in enumerate(sequences):
+		indices.extend(zip([n]*len(seq), range(len(seq))))
+		values.extend(seq)
+	
+	indices = np.asarray(indices, dtype=np.int64)
+	values = np.asarray(values, dtype=dtype)
+	shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1]+1], dtype=np.int64)
+	
+	return indices, values, shape
 
 def drop_outliers(dataframe, n, features):
 	"""
@@ -152,7 +250,7 @@ def drop_outliers(dataframe, n, features):
 		outlier_indices.extend(outlier_list_col)
 	
 	# select observations containing more than 2 outliers
-	outlier_indices = Counter(outlier_indices)        
+	outlier_indices = Counter(outlier_indices)		
 	outliers = list( k for k, v in outlier_indices.items() if v > n )
 	
 	print 'drop outlier rows:%s'%str(outliers)
